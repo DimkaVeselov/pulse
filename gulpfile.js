ec80063bf431434baea37b0c990f3ec09a573ff4
Dmitrii Privezencev <dv.privezencev@mail.ru>
2022-11-13 20:20:31 +0300

    gulp and dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function () {
 	browserSync({
 		server: {
-			baseDir: 'src',
+			baseDir: 'dist',
 		},
 	});
 
@@ -22,12 +24,56 @@ gulp.task('styles', function () {
 		.pipe(rename({ suffix: '.min', prefix: '' }))
 		.pipe(autoprefixer())
 		.pipe(cleanCSS({ compatibility: 'ie8' }))
-		.pipe(gulp.dest('src/css'))
+		.pipe(gulp.dest('dist/css'))
 		.pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
-	gulp.watch('src/scss/**/*.+(scss|sass)', gulp.parallel('styles'));
+	gulp.watch('src/scss/**/*.+(scss|sass|css)', gulp.parallel('styles'));
+	gulp.watch('src/*.html').on('change', gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function () {
+	return gulp
+		.src('src/*.html')
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('scripts', function () {
+	return gulp.src('src/js/**/*.js').pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('fonts', function () {
+	return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('icon', function () {
+	return gulp.src('src/icon/**/*').pipe(gulp.dest('dist/icon'));
+});
+
+gulp.task('mailer', function () {
+	return gulp.src('src/mailer/**/*').pipe(gulp.dest('dist/mailer'));
+});
+
+gulp.task('image', function () {
+	return gulp
+		.src('src/image/**/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/image'));
+});
+
+gulp.task(
+	'default',
+	gulp.parallel(
+		'watch',
+		'server',
+		'styles',
+		'html',
+		'scripts',
+		'fonts',
+		'icon',
+		'mailer',
+		'image',
+	),
+);
